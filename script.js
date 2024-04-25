5ae4fac52f8c84fae5b95d1c064544e41083122c
Dmitry Kravcev <tpdigmma@gmail.com>
2024-04-25 14:20:56 +0300

    first commit in lesson01

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-l
+// Объявление переменных
+let title, screens, screenPrice, rollback,fullPrice, adaptive;
+
+// Вывод в модальное окно
+alert("Добро пожаловать в наш проект!");
+
+// Вывод в консоль
+console.log("Запуск проекта выполнен успешно.");
